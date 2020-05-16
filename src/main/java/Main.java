78019373f9e70381f556049d7b3be109c602bda4
Nick97-git <nickarchangel97@gmail.com>
2020-05-16 17:38:56 +0300

    added solution

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean bool = true;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (bool) {
+                System.out.println("Enter string for matrix: ");
+                String matrixString = scanner.nextLine();
+                System.out.println("Enter your word: ");
+                char[] word = scanner.nextLine().toCharArray();
+                if (Math.sqrt(matrixString.length()) % 1 == 0
+                        && word.length <= matrixString.length()
+                        && matrixString.length() != 0 && word.length != 0) {
+                    WordSearcher wordSearcher = new WordSearcher(word);
+                    char[][] matrix = wordSearcher.fillMatrix(matrixString);
+                    System.out.println("Filled matrix: ");
+                    for (char[] cells : matrix) {
+                        for (char cell : cells) {
+                            System.out.print(cell + " ");
+                        }
+                        System.out.println();
+                    }
+                    wordSearcher.findWord();
+                    System.out.println("Do you wanna try again? Enter yes or no");
+                    String answer = scanner.nextLine();
+                    if (answer.equals("no")) {
+                        bool = false;
+                    }
+                } else {
+                    System.out.println("You entered incorrect data! Please enter data again)");
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/WordSearcher.java
@@ -0,0 +1,61 @@
+public class WordSearcher {
+    private final int[] rowNum = {-1, 1, 0, 0};
+    private final int[] colNum = {0, 0, -1, 1};
+    private final char[] word;
+    private char[][] matrix;
+    private int length;
+
+    public WordSearcher(char[] word) {
+        this.word = word;
+    }
+
+    public char[][] fillMatrix(String matrixString) {
+        length = (int) Math.sqrt(matrixString.length());
+        String[] splitString = matrixString.split("(?<=\\G.{" + length + "})");
+        matrix = new char[length][length];
+        for (int i = 0; i < splitString.length; i++) {
+            matrix[i] = splitString[i].toCharArray();
+        }
+        return matrix;
+    }
+
+    public void findWord() {
+        for (int i = 0; i < length; ++i) {
+            for (int j = 0; j < length; ++j) {
+                if (matrix[i][j] == word[0]) {
+                    if (isWordInMatrix(i, j, "", 0, word.length - 1)) {
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("Can't find word " + new String(word) + " in matrix");
+    }
+
+    private boolean isWordInMatrix(int row, int column, String path, int index, int wordLength) {
+        if (index > wordLength || matrix[row][column] != word[index]) {
+            return false;
+        }
+
+        path += "[" + row + ", " + column + "]";
+
+        if (index == wordLength) {
+            System.out.print("Answer: " + path.replaceAll("]\\[", "]->[") + "\n");
+            return true;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (isValid(row + rowNum[i], column + colNum[i])) {
+                if (isWordInMatrix(row + rowNum[i], column + colNum[i],
+                        path, index + 1, wordLength)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isValid(int row, int col) {
+        return (row >= 0) && (row < length) && (col >= 0) && (col < length);
+    }
+}
