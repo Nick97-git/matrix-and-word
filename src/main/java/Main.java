0a0cc2c3c059a7446b64e89004bf700aedbdc2a6
Nick97-git <nickarchangel97@gmail.com>
2020-05-18 14:17:26 +0300

    removed fields in WordSearcher, changed type of returned value in findWord and getWordInMatrix to String, create private method for printing of matrix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        WordSearcher wordSearcher = new WordSearcher();
         boolean bool = true;
         try (Scanner scanner = new Scanner(System.in)) {
             while (bool) {
@@ -10,21 +11,14 @@ public class Main {
                 String matrixString = scanner.nextLine();
                 System.out.println("Enter your word: ");
                 char[] word = scanner.nextLine().toCharArray();
-                if (Math.sqrt(matrixString.length()) % 1 == 0
-                        && word.length <= matrixString.length()
-                        && matrixString.length() != 0 && word.length != 0) {
+                if (matrixString.length() != 0 && word.length != 0
+                        && Math.sqrt(matrixString.length()) % 1 == 0
+                        && word.length <= matrixString.length()) {
                     int length = (int) Math.sqrt(matrixString.length());
-                    WordSearcher wordSearcher = new WordSearcher(word, length);
-                    wordSearcher.fillMatrix(matrixString);
-                    char[][] matrix = wordSearcher.getMatrix();
+                    char[][] matrix = wordSearcher.getFillMatrix(matrixString, length);
                     System.out.println("Filled matrix: ");
-                    for (char[] cells : matrix) {
-                        for (char cell : cells) {
-                            System.out.print(cell + " ");
-                        }
-                        System.out.println();
-                    }
-                    wordSearcher.findWord();
+                    printMatrix(matrix);
+                    System.out.println(wordSearcher.findWord(word, matrix, length));
                     System.out.println("Do you wanna try again? Enter yes or no");
                     String answer = scanner.nextLine();
                     if (answer.equals("no")) {
@@ -34,6 +28,15 @@ public class Main {
                     System.out.println("You entered incorrect data! Please enter data again)");
                 }
             }
+        }
+    }
+
+    private static void printMatrix(char[][] matrix) {
+        for (char[] cells : matrix) {
+            for (char cell : cells) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
         }
     }
 }
--- src/main/java/WordSearcher.java
+++ src/main/java/WordSearcher.java
@@ -1,64 +1,55 @@
 public class WordSearcher {
     private final int[] rowNum = {-1, 1, 0, 0};
     private final int[] colNum = {0, 0, -1, 1};
-    private final char[] word;
-    private final char[][] matrix;
-    private final int length;
 
-    public WordSearcher(char[] word, int length) {
-        this.word = word;
-        this.length = length;
-        this.matrix = new char[length][length];
-    }
-
-    public void fillMatrix(String matrixString) {
+    public char[][] getFillMatrix(String matrixString, int length) {
         String[] splitString = matrixString.split("(?<=\\G.{" + length + "})");
+        char[][] matrix = new char[length][length];
         for (int i = 0; i < splitString.length; i++) {
             matrix[i] = splitString[i].toCharArray();
         }
-    }
-
-    public char[][] getMatrix() {
         return matrix;
     }
 
-    public void findWord() {
+    public String findWord(char[] word, char[][] matrix, int length) {
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 if (matrix[i][j] == word[0]) {
-                    if (isWordInMatrix(i, j, "", 0)) {
-                        return;
+                    String answer = getWordInMatrix(i, j, "", 0, word, matrix, length);
+                    if (answer.length() != 0) {
+                        return answer;
                     }
                 }
             }
         }
-        System.out.println("Can't find word " + new String(word) + " in matrix");
+        return "Can't find word " + new String(word) + " in matrix";
     }
 
-    private boolean isWordInMatrix(int row, int column, String path, int index) {
+    private String getWordInMatrix(int row, int column, String path,
+                                   int index, char[] word, char[][] matrix, int length) {
         if (matrix[row][column] != word[index]) {
-            return false;
+            return "";
         }
 
         path += "[" + row + ", " + column + "]";
 
         if (index == word.length - 1) {
-            System.out.print("Answer: " + path.replaceAll("]\\[", "]->[") + "\n");
-            return true;
+            return "Answer: " + path.replaceAll("]\\[", "]->[");
         }
 
         for (int i = 0; i < rowNum.length; i++) {
-            if (isValid(row + rowNum[i], column + colNum[i])) {
-                if (isWordInMatrix(row + rowNum[i], column + colNum[i],
-                        path, index + 1)) {
-                    return true;
+            if (isValid(row + rowNum[i], column + colNum[i], length)) {
+                String answer = getWordInMatrix(row + rowNum[i], column + colNum[i],
+                        path, index + 1, word, matrix, length);
+                if (answer.length() != 0) {
+                    return answer;
                 }
             }
         }
-        return false;
+        return "";
     }
 
-    private boolean isValid(int row, int col) {
+    private boolean isValid(int row, int col, int length) {
         return (row >= 0) && (row < length) && (col >= 0) && (col < length);
     }
 }
